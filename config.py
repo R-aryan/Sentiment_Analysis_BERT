782bb362a348ce16467671bb8f98f6d8f5cd2824
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-17 20:50:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
-
+import transformers
 
 MAX_LEN=512
+TRAIN_BATCH_SIZE=8
+VALID_BATCH_SIZE=4
+EPOCHS=10
+BERT_PATH="input/bert_base_uncased/"
+MODEL_PATH = "model.bin"
+TRAINING_FILE = "input/imdb.csv"
+
