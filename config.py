57e1e56aafe96a89b64694d40ecded7f9cb88c90
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-18 23:39:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,8 @@ BERT_PATH="input/bert_base_uncased/"
 MODEL_PATH = "model.bin"
 TRAINING_FILE = "input/imdb.csv"
 
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BERT_PATH,
+    do_lower_case=True
+)
+
