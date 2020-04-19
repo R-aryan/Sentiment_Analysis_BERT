1fb5fb688c6c343bb84e63cf8f658ea501bd4f3d
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-19 16:49:26 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,3 +3,13 @@ import config
 
 
 class BERTDataset():
+
+    def __init__(self,review,target):
+        self.review=review
+        self.target=target
+        self.tokenizer=config.TOKENIZER
+        self.max_len=config.MAX_LEN
+
+
+    def __len__(self):
+        return len(self.review)
