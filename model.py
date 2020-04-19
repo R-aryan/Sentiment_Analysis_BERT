a34f9cc334095c94e1dd1c2f158eff85876edbd3
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-19 13:40:30 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,18 @@
 import torch.nn as nn
 import transformers
+import config
 
 class BERTBaseUncased(nn.Module):
-    
+
+    ##defining the init function
+    def __init__(self):
+
+        super(BERTBaseUncased,self).__init__()
+        self.bert=transformers.BertModel.from_pretrained(config.BERT_PATH)
+        self.bert_drop=nn.Dropout(0.3)
+        self.out=nn.Linear(768,1)
+
+
+
+
+
