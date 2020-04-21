f83f168c507a19c9d8e47201068808f2aaa2588e
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-21 12:25:25 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -30,15 +30,41 @@ def train_fn(data_loader,model,optimizer,device,schedular):
         )
 
         loss=loss_fn(outputs,targets)
+        loss.backward()
+        optimizer.step()
+        schedular.step()
 
 
 
 
 
+def eval_fn(data_loader,model):
+    model.eval()
+    fin_targets=[]
+    fin_outputs=[]
+
+    with torch.no_grad():
+        for bi,d in tqdm(enumerate(data_loader),total=len(data_loader)):
+
+        ids=d["ids"]
+        token_type_ids=d["token_type_ids"]
+        mask=d["mask"]
+        targets=d["targets"]
+
+        ids= ids.to(device,dtype=torch.long)
+        token_type_ids= token_type_ids.to(device,dtype=torch.long)
+        mask= mask.to(device,dtype=torch.long)
+        targets= targets.to(device,dtype=torch.long)
+
+        optimizer.zero_grad()
+
+        outputs= model(
+            ids=ids,
+            mask=mask,
+            token_type_ids=token_type_ids
+        )
+
+        fin_targets.extend(targets.cpu().detach().numpy().tolist())
+        fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
 
 
-
-
-
-def eval_fn():
-    pass
