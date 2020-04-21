cdccfcbbe6aba5b1517b6064169510e0689acb11
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-21 12:26:46 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -67,4 +67,7 @@ def eval_fn(data_loader,model):
         fin_targets.extend(targets.cpu().detach().numpy().tolist())
         fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
 
+    return fin_outputs,fin_targets
+
+
 
