9535f6b898fe3d7994cb0e539ddfcb774065f81b
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-22 12:30:15 +0530

    Update train.py

--- train.py
+++ train.py
@@ -11,3 +11,26 @@ from sklearn import model_selection
 from sklearn import metrics
 from transformers import AdamW
 from transformers import get_linear_schedule_with_warmup
+
+
+
+def run():
+    #reading the dataseet
+    dfx= pd.read_csv(config.TRAINING_FILE).fillna("none")
+
+    dfx.sentiment= dfx.sentiment.apply(
+        lambda x:1 if x=="positive" else 0
+    )
+
+    
+
+    #splitting into training and validation set
+    df_train,df_valid= model_selection.train_test_split(
+        dfx,
+        test_size=0.1,
+        random_state=42,
+        stratify= dfx.sentiment.values
+    )
+
+    df_train= df_train.reset_index(drop=True)
+    df_valid= df_valid.reset_index(drop=True)
