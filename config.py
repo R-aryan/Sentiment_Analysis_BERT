6ef9a19e821720e1185127093d023f8e98a1172e
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-29 10:47:53 +0530

    added files

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ MAX_LEN=512
 TRAIN_BATCH_SIZE=8
 VALID_BATCH_SIZE=4
 EPOCHS=10
-BERT_PATH="input/bert_base_uncased/"
+BERT_PATH="input/bert-base-uncased/"
 MODEL_PATH = "model.bin"
 TRAINING_FILE = "input/imdb.csv"
 
