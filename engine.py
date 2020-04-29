eb07c03e42437464f3c1e55c2046bda1861fabe7
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-04-29 10:57:59 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -35,11 +35,10 @@ def train_fn(data_loader, model, optimizer, device, schedular):
         optimizer.step()
         schedular.step()
 
-def eval_fn(data_loader, model):
+def eval_fn(data_loader, model, device):
     model.eval()
     fin_targets = []
     fin_outputs = []
-
     with torch.no_grad():
         for bi, d in tqdm(enumerate(data_loader), total=len(data_loader)):
             ids = d["ids"]
@@ -50,19 +49,13 @@ def eval_fn(data_loader, model):
             ids = ids.to(device, dtype=torch.long)
             token_type_ids = token_type_ids.to(device, dtype=torch.long)
             mask = mask.to(device, dtype=torch.long)
-            targets = targets.to(device, dtype=torch.long)
-
-            optimizer.zero_grad()
+            targets = targets.to(device, dtype=torch.float)
 
             outputs = model(
-               ids=ids,
-               mask=mask,
-               token_type_ids=token_type_ids
+                ids=ids,
+                mask=mask,
+                token_type_ids=token_type_ids
             )
-
             fin_targets.extend(targets.cpu().detach().numpy().tolist())
-            fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist()
-                           )
-
+            fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
     return fin_outputs, fin_targets
-
