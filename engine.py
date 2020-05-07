e919b80148696bb1638c8e2702305967921a82b3
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-05-07 15:47:55 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm
 
-
 def loss_fn(outputs, targets):
     return nn.BCEWithLogitsLoss()(outputs, targets.view(-1, 1))
 
@@ -20,7 +19,7 @@ def train_fn(data_loader, model, optimizer, device, schedular):
         ids = ids.to(device, dtype=torch.long)
         token_type_ids = token_type_ids.to(device, dtype=torch.long)
         mask = mask.to(device, dtype=torch.long)
-        targets = targets.to(device, dtype=torch.long)
+        targets = targets.to(device, dtype=torch.float)
 
         optimizer.zero_grad()
 
