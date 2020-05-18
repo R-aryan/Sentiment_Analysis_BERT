ef487b61054982f745336bbd50fcf2d5226c2f4a
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-05-18 11:08:41 +0530

    Update config.py

--- src/Training/config.py
+++ src/Training/config.py
@@ -4,9 +4,9 @@ MAX_LEN=512
 TRAIN_BATCH_SIZE=8
 VALID_BATCH_SIZE=4
 EPOCHS=10
-BERT_PATH="input/bert-base-uncased/"
+BERT_PATH="../input/bert-base-uncased/"
 MODEL_PATH = "model.bin"
-TRAINING_FILE = "input/IMDB Dataset.csv"
+TRAINING_FILE = "../input/IMDB Dataset.csv"
 
 TOKENIZER = transformers.BertTokenizer.from_pretrained(
     BERT_PATH,
