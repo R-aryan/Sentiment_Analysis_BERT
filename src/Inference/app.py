4217bbb16296c670165fb16eccef2de4e2a23de9
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-05-20 16:44:56 +0530

    Update app.py

--- src/Inference/app.py
+++ src/Inference/app.py
@@ -8,3 +8,11 @@ from model import BERTBaseUncased
 import functools
 import torch.nn as nn
 import joblib
+
+app = Flask(__name__)
+
+
+MODEL = None
+DEVICE = "cpu"
+PREDICTION_DICT = dict()
+memory = joblib.Memory("../input/", verbose=0)
