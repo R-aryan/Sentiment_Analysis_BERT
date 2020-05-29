716d4ea7b7560b803c0899f1941aa9075c85c84c
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-05-29 11:33:10 +0530

    Create model.py

---
+++ src/Inference/model.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import transformers
+import config
+
+class BERTBaseUncased(nn.Module):
+
+    ##defining the init function
+    def __init__(self):
+
+        super(BERTBaseUncased,self).__init__()
+        self.bert=transformers.BertModel.from_pretrained(config.BERT_PATH)
+        self.bert_drop=nn.Dropout(0.3)
+        self.out=nn.Linear(768,1)
+
+
+    def forward(self,ids,mask,token_type_ids):
+
+        o1,o2=self.bert(
+            ids,
+            attention_mask=mask,
+            token_type_ids=token_type_ids
+        )
+
+        bo=self.bert_drop(o2)
+        output=self.out(bo)
+
+        return output
+
+
+
+
+
+
